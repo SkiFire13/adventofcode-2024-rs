8fbea2f980b69cf2bf3e9542809f673cf1fe4012
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-15 17:52:01 +0100

    Improve day 15 solution

--- src/day15.rs
+++ src/day15.rs
@@ -34,7 +34,9 @@ pub fn part1(input: &Input) -> usize {
                     grid[(x, y)] = b'.';
                     break;
                 }
-                b'#' => break,
+                b'#' => {
+                    break;
+                }
                 _ => unreachable!(),
             }
         }
@@ -68,7 +70,7 @@ pub fn part2(input: &Input) -> usize {
     }
 
     let (mut x, mut y) = (2 * *x as isize, *y as isize);
-    for &op in *ops {
+    'outer: for &op in *ops {
         let (dx, dy) = match op {
             b'^' => (0, -1),
             b'v' => (0, 1),
@@ -78,132 +80,40 @@ pub fn part2(input: &Input) -> usize {
             _ => unreachable!(),
         };
 
-        fn move2(grid: &mut Grid<u8>, x: isize, y: isize, dx: isize, dy: isize) -> bool {
-            match grid[(x, y)] {
-                b'#' => false,
-                b'.' => true,
-                b'[' => {
-                    if dx == 0 {
-                        if move2(grid, x, y + dy, dx, dy) && move2(grid, x + 1, y + dy, dx, dy) {
-                            grid[(x, y)] = b'.';
-                            grid[(x + 1, y)] = b'.';
-                            grid[(x, y + dy)] = b'[';
-                            grid[(x + 1, y + dy)] = b']';
-                            true
-                        } else {
-                            false
-                        }
-                    } else {
-                        if move2(grid, x + 2, y, dx, dy) {
-                            grid[(x, y)] = b'.';
-                            grid[(x + 1, y)] = b'[';
-                            grid[(x + 2, y)] = b']';
-                            true
-                        } else {
-                            false
+        let (nx, ny) = (x + dx, y + dy);
+
+        if grid[(nx, ny)] != b'.' {
+            let mut to_move = FxIndexSet::from_iter([(nx, ny)]);
+            let mut index = 0;
+
+            while let Some(&(x, y)) = to_move.get_index(index) {
+                index += 1;
+                match grid[(x, y)] {
+                    b'#' => continue 'outer,
+                    b'.' => {}
+                    b'[' => {
+                        to_move.insert((x + 1, y));
+                        if grid[(x + dx, y + dy)] != b'.' {
+                            to_move.insert((x + dx, y + dy));
                         }
                     }
-                }
-                b']' => {
-                    if dx == 0 {
-                        if move2(grid, x, y + dy, dx, dy) && move2(grid, x - 1, y + dy, dx, dy) {
-                            grid[(x, y)] = b'.';
-                            grid[(x - 1, y)] = b'.';
-                            grid[(x, y + dy)] = b']';
-                            grid[(x - 1, y + dy)] = b'[';
-                            true
-                        } else {
-                            false
-                        }
-                    } else {
-                        if move2(grid, x - 2, y, dx, dy) {
-                            grid[(x - 2, y)] = b'[';
-                            grid[(x - 1, y)] = b']';
-                            grid[(x, y)] = b'.';
-                            true
-                        } else {
-                            false
+                    b']' => {
+                        to_move.insert((x - 1, y));
+                        if grid[(x + dx, y + dy)] != b'.' {
+                            to_move.insert((x + dx, y + dy));
                         }
                     }
+                    _ => unreachable!(),
                 }
-                _ => unreachable!(),
+            }
+
+            for &(x, y) in to_move.iter().rev() {
+                grid[(x + dx, y + dy)] = grid[(x, y)];
+                grid[(x, y)] = b'.';
             }
         }
 
-        let mut new_grid = grid.clone();
-        if move2(&mut new_grid, x + dx, y + dy, dx, dy) {
-            for ((x, y), &c) in new_grid.iter() {
-                if c == b'[' {
-                    assert_eq!(new_grid[(x + 1, y)], b']');
-                }
-            }
-            grid = new_grid;
-            (x, y) = (x + dx, y + dy);
-        }
-
-        // fn check_move(grid: &Grid<u8>, x: isize, y: isize, dx: isize, dy: isize, r: bool) -> bool {
-        //     match grid[(x, y)] {
-        //         b'#' => false,
-        //         b'.' => true,
-        //         b'[' => {
-        //             if dx == 0 {
-        //                 if r && !check_move(grid, x + 1, y, dx, dy, false) {
-        //                     return false;
-        //                 }
-        //                 check_move(grid, x + dx, y + dy, dx, dy, true)
-        //             } else {
-        //                 check_move(grid, x + 2 * dx, y + 2 * dy, dx, dy, false)
-        //             }
-        //         }
-        //         b']' => {
-        //             if dx == 0 {
-        //                 if r && !check_move(grid, x - 1, y, dx, dy, false) {
-        //                     return false;
-        //                 }
-        //                 check_move(grid, x + dx, y + dy, dx, dy, true)
-        //             } else {
-        //                 check_move(grid, x + 2 * dx, y + 2 * dy, dx, dy, false)
-        //             }
-        //         }
-        //         _ => unreachable!(),
-        //     }
-        // }
-        // fn do_move(grid: &mut Grid<u8>, x: isize, y: isize, dx: isize, dy: isize, c: u8, r: bool) {
-        //     match grid[(x, y)] {
-        //         b'#' => unreachable!(),
-        //         b'.' => grid[(x, y)] = c,
-        //         b'[' => {
-        //             if dx == 0 {
-        //                 if r {
-        //                     do_move(grid, x + 1, y, dx, dy, b'.', false);
-        //                 }
-        //                 grid[(x, y)] = c;
-        //                 do_move(grid, x + dx, y + dy, dx, dy, b'[', true);
-        //             } else {
-        //                 grid[(x + dx, y + dy)] = b'[';
-        //                 do_move(grid, x + 2 * dx, y + 2 * dy, dx, dy, b']', false);
-        //             }
-        //         }
-        //         b']' => {
-        //             if dx == 0 {
-        //                 if r {
-        //                     do_move(grid, x - 1, y, dx, dy, b'.', false);
-        //                 }
-        //                 grid[(x, y)] = c;
-        //                 do_move(grid, x + dx, y + dy, dx, dy, b'[', true);
-        //             } else {
-        //                 grid[(x + dx, y + dy)] = b']';
-        //                 do_move(grid, x + 2 * dx, y + 2 * dy, dx, dy, b'[', false);
-        //             }
-        //         }
-        //         _ => unreachable!(),
-        //     }
-        // }
-
-        // if check_move(&grid, x + dx, y + dy, dx, dy, true) {
-        //     do_move(&mut grid, x + dx, y + dy, dx, dy, b'.', true);
-        //     (x, y) = (x + dx, y + dy);
-        // }
+        (x, y) = (nx, ny);
     }
 
     grid.iter().filter(|(_, &c)| c == b'[').map(|((x, y), _)| 100 * y + x).sum()
