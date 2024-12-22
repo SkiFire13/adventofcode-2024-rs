ee2c9c69b66c5a24ca947494ac8b3c5670dbb7a1
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-12-22 06:46:29 +0100

    Parallelize day 22 solution

--- src/day22.rs
+++ src/day22.rs
@@ -27,38 +27,59 @@ pub fn part1(input: &Input) -> u64 {
 }
 
 pub fn part2(input: &Input) -> u64 {
-    let mut local_counts = FxHashSet::default();
-    let mut counts = FxHashMap::default();
+    let cores = std::thread::available_parallelism().unwrap().get();
+    input
+        .par_chunks((input.len() + cores - 1) / cores)
+        .with_max_len(1)
+        .map_init(
+            || FxHashSet::default(),
+            |seen, chunk| {
+                let mut counts = FxHashMap::default();
 
-    for &(mut n) in input {
-        local_counts.clear();
+                for &(mut n) in chunk {
+                    seen.clear();
 
-        let b1 = (n % 10) as i8;
-        n = next(n);
-        let b2 = (n % 10) as i8;
-        n = next(n);
-        let b3 = (n % 10) as i8;
-        n = next(n);
-        let mut b4 = (n % 10) as i8;
+                    let b1 = (n % 10) as i8;
+                    n = next(n);
+                    let b2 = (n % 10) as i8;
+                    n = next(n);
+                    let b3 = (n % 10) as i8;
+                    n = next(n);
+                    let mut b4 = (n % 10) as i8;
 
-        let mut d1 = b1 - b2;
-        let mut d2 = b2 - b3;
-        let mut d3 = b3 - b4;
+                    let mut d1 = b1 - b2;
+                    let mut d2 = b2 - b3;
+                    let mut d3 = b3 - b4;
 
-        for _ in 3..2000 {
-            n = next(n);
-            let b5 = (n % 10) as i8;
+                    for _ in 3..2000 {
+                        n = next(n);
+                        let b5 = (n % 10) as i8;
 
-            let d4 = b4 - b5;
+                        let d4 = b4 - b5;
 
-            let w = u32::from_ne_bytes([d1, d2, d3, d4].map(|b| b as u8));
-            if local_counts.insert(w) {
-                *counts.entry(w).or_insert(0) += b5 as u64;
+                        let w = u32::from_ne_bytes([d1, d2, d3, d4].map(|b| b as u8));
+                        if seen.insert(w) {
+                            *counts.entry(w).or_insert(0) += b5 as u64;
+                        }
+
+                        (d1, d2, d3, b4) = (d2, d3, d4, b5);
+                    }
+                }
+
+                counts
+            },
+        )
+        .reduce_with(|mut counts1, mut counts2| {
+            if counts1.len() < counts2.len() {
+                std::mem::swap(&mut counts1, &mut counts2);
             }
-
-            (d1, d2, d3, b4) = (d2, d3, d4, b5);
-        }
-    }
-
-    counts.into_values().max().unwrap()
+            for (k, v) in counts2 {
+                *counts1.entry(k).or_insert(0) += v;
+            }
+            counts1
+        })
+        .unwrap()
+        .into_values()
+        .max()
+        .unwrap()
 }
